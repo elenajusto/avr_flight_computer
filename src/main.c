ae3e8d5d68909d8baad1de5f106522e7706e7870
Elena <elenajusto@users.noreply.github.com>
2024-01-23 16:44:26 +1100

    Initial main.c code structure

---
+++ src/main.c
@@ -0,0 +1,11 @@
+// Constants
+#define F_CPU 16000000UL    // Arduino board clock frequency: 16 Mhz
+
+// Header Files
+#include <avr/io.h>         // Pin definitions for AVR
+#include <util/delay.h>     // Delay functions for AVR
+
+// Main Program
+int main(void) {
+
+}
